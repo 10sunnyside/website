b9401e091eb6ef6abac52eea688d338205b1023e
10sunnyside <61858515+10sunnyside@users.noreply.github.com>
2020-06-26 14:00:52 +0900

    layout

--- js/new-age.js
+++ js/new-age.js
@@ -62,6 +62,37 @@ var i = 0;
   }
 }*/
 
+/*
+$(document).ready(function() {
+  $(window).scroll( function(){
+      $('.bg-primary').each( function(i){
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          if( bottom_of_window > bottom_of_object/2 ){
+              $(this).animate({'opacity':'1'},500);
+          }
+      }); 
+  });
+});
+*/
+
+$(document).ready(function() {
+  $(window).scroll( function(){
+      $('.bg-primary').each( function(i){
+          
+          var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+          if( bottom_of_window > bottom_of_element ){
+              $(this).animate({'opacity':'1','margin-left':'0px'},1000);
+          }
+          
+      }); 
+  });
+});
+
+
+
 setInterval(function(){
   $('#1p').text(plist[i]);
 
