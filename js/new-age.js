581384712c3a8868ded7c38366ce1859c1f8c275
10sunnyside <61858515+10sunnyside@users.noreply.github.com>
2021-01-29 22:27:37 +0900

    homepage

--- js/new-age.js
+++ js/new-age.js
@@ -141,7 +141,7 @@ $(document).ready(function() {
       $('.hideme').each( function(i){
           
           var bottom_of_object = $(this).position().top + $(this).outerHeight();
-          var bottom_of_window = $(window).scrollTop() + $(window).height()-$(window).height()/2;
+          var bottom_of_window = $(window).scrollTop() + $(window).height()+$(window).height()/2;
           
           /* If the object is completely visible in the window, fade it it */
           if( bottom_of_window > bottom_of_object ){
